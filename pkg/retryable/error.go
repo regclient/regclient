965f11eb304531c250bb01bc3928b645235abef8
Brandon Mitchell <git@bmitch.net>
2020-08-22 13:33:46 -0400

    Adding retryable package [WIP]

---
+++ pkg/retryable/error.go
@@ -0,0 +1,18 @@
+package retryable
+
+import "errors"
+
+var (
+	// ErrBackoffLimit maximum backoff attempts reached
+	ErrBackoffLimit = errors.New("Backoff limit reached")
+	// ErrDigestMismatch if the expected digest wasn't received
+	ErrDigestMismatch = errors.New("Digest mismatch")
+	// ErrNotFound isn't there, search for your value elsewhere
+	ErrNotFound = errors.New("Not found")
+	// ErrNotImplemented returned when method has not been implemented yet
+	ErrNotImplemented = errors.New("Not implemented")
+	// ErrRetryNeeded indicates a request needs to be retried
+	ErrRetryNeeded = errors.New("Retry needed")
+	// ErrUnauthorized request was not authorized
+	ErrUnauthorized = errors.New("Unauthorized")
+)
---
+++ pkg/retryable/retryable.go
@@ -0,0 +1,393 @@
+package retryable
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"os"
+	"time"
+
+	digest "github.com/opencontainers/go-digest"
+)
+
+// Retryable is used to create requests with built in retry capabilities
+type Retryable interface {
+	DoRequest(ctx context.Context, method string, u url.URL, opts ...OptsReq) (Response, error)
+}
+
+// Response is used to handle the result of a request
+type Response interface {
+	io.ReadCloser
+	HTTPResponse() *http.Response
+	HTTPResponses() ([]*http.Response, error)
+}
+
+// Auth is used to process Www-Authenticate header and update request with Authorization header
+type Auth interface {
+	HandleResponse(*http.Response) error
+	UpdateRequest(*http.Request) error
+}
+
+// Opts injects options into NewRetryable
+type Opts func(*retryable)
+
+// OptsReq injects options into NewRequest
+type OptsReq func(*request)
+
+type retryable struct {
+	httpClient *http.Client
+	mirrorFunc func(url.URL) ([]url.URL, error)
+	auth       Auth
+	limit      int
+	delayInit  time.Duration
+	delayMax   time.Duration
+}
+
+// NewRetryable returns a retryable interface
+func NewRetryable(opts ...Opts) Retryable {
+	r := &retryable{
+		httpClient: &http.Client{},
+		limit:      5,
+	}
+	r.delayInit, _ = time.ParseDuration("1s")
+	r.delayMax, _ = time.ParseDuration("30s")
+
+	for _, opt := range opts {
+		opt(r)
+	}
+
+	return r
+}
+
+// WithAuth adds authentication to retryable methods
+func WithAuth(auth Auth) Opts {
+	return func(r *retryable) {
+		r.auth = auth
+	}
+}
+
+// WithDelay initial time to wait between retries (increased with exponential backoff)
+func WithDelay(delayInit time.Duration, delayMax time.Duration) Opts {
+	return func(r *retryable) {
+		if delayInit > 0 {
+			r.delayInit = delayInit
+		} else {
+			r.delayInit = 0
+		}
+		// delayMax must be at least delayInit, if 0 initialize to 30x delayInit
+		if delayMax > r.delayInit {
+			r.delayMax = delayMax
+		} else if delayMax > 0 {
+			r.delayMax = r.delayInit
+		} else {
+			r.delayMax = r.delayInit * 30
+		}
+	}
+}
+
+// WithHTTPClient uses a specific http client with retryable requests
+func WithHTTPClient(h *http.Client) Opts {
+	return func(r *retryable) {
+		r.httpClient = h
+	}
+}
+
+// WithLimit restricts the number of retries (defaults to 5)
+func WithLimit(l int) Opts {
+	return func(r *retryable) {
+		r.limit = l
+	}
+}
+
+// WithMirrors adds ability to contact mirrors in retryable methods
+func WithMirrors(mirrorFunc func(url.URL) ([]url.URL, error)) Opts {
+	return func(r *retryable) {
+		r.mirrorFunc = mirrorFunc
+	}
+}
+
+// WithTransport uses a specific http transport with retryable requests
+func WithTransport(t *http.Transport) Opts {
+	return func(r *retryable) {
+		r.httpClient = &http.Client{Transport: t}
+	}
+}
+
+type request struct {
+	r          *retryable
+	method     string
+	urls       []url.URL
+	curURL     int
+	header     http.Header
+	getBody    func() (io.ReadCloser, error)
+	contentLen int64
+	chunking   bool
+	offset     int64
+	curRead    int64
+	backoffs   int
+	done       bool
+	digest     digest.Digest
+	digester   digest.Digester
+	progressCB func(int64, error)
+	responses  []*http.Response
+}
+
+func (r *retryable) DoRequest(ctx context.Context, method string, u url.URL, opts ...OptsReq) (Response, error) {
+	req := &request{
+		r:          r,
+		method:     method,
+		urls:       []url.URL{u},
+		curURL:     0,
+		header:     http.Header{},
+		getBody:    nil,
+		contentLen: -1,
+		chunking:   false,
+		offset:     0,
+		curRead:    0,
+		backoffs:   0,
+		done:       false,
+		digest:     "",
+		digester:   nil,
+		progressCB: nil,
+		responses:  []*http.Response{},
+	}
+	// replace url list with mirrors
+	if r.mirrorFunc != nil {
+		if urls, err := r.mirrorFunc(u); err == nil {
+			req.urls = urls
+		}
+	}
+	// apply opts
+	for _, opt := range opts {
+		opt(req)
+	}
+
+	// run the request until successful or non-recoverable error
+	for {
+		if err := req.httpDo(); err != nil {
+			return req, err
+		}
+		err := req.checkResp()
+		if err == nil {
+			break
+		} else if err != ErrRetryNeeded {
+			return req, err
+		}
+	}
+
+	return req, nil
+}
+
+// WithBodyBytes converts a bytes slice into a body func and content length
+func WithBodyBytes(body []byte) OptsReq {
+	return func(req *request) {
+		req.contentLen = int64(len(body))
+		req.getBody = func() (io.ReadCloser, error) {
+			return ioutil.NopCloser(bytes.NewReader(body)), nil
+		}
+	}
+}
+
+// WithBodyFunc includes body content in a request
+func WithBodyFunc(getbody func() (io.ReadCloser, error)) OptsReq {
+	return func(req *request) {
+		req.getBody = getbody
+	}
+}
+
+// WithChunking allows content to be divided into multiple smaller chunks
+func WithChunking() OptsReq {
+	return func(req *request) {
+		req.chunking = true
+	}
+}
+
+// WithContentLen sets the content length
+func WithContentLen(l int64) OptsReq {
+	return func(req *request) {
+		req.contentLen = l
+	}
+}
+
+// WithDigest verifies the returned content digest matches.
+// Note that the digest is only calculated upon EOF from the downloaded
+// content, so the reader may receive an error rather than EOF from a
+// digest mismatch. The content itself must still be read.
+func WithDigest(d digest.Digest) OptsReq {
+	return func(req *request) {
+		req.digest = d
+		req.digester = digest.Canonical.Digester()
+	}
+}
+
+// WithHeader sets a header
+func WithHeader(key string, values []string) OptsReq {
+	return func(req *request) {
+		for _, v := range values {
+			req.header.Add(key, v)
+		}
+	}
+}
+
+// WithProgressCB calls the CB function as data is received
+func WithProgressCB(cb func(int64, error)) OptsReq {
+	return func(req *request) {
+		req.progressCB = cb
+	}
+}
+
+func (req *request) httpDo() error {
+	// build the http reqest for the current mirror url
+	httpReq, err := http.NewRequest(req.method, req.urls[req.curURL].String(), nil)
+	if err != nil {
+		return err
+	}
+	if req.getBody != nil {
+		httpReq.Body, err = req.getBody()
+		if err != nil {
+			return err
+		}
+		httpReq.GetBody = req.getBody
+		httpReq.ContentLength = req.contentLen
+	}
+	if len(req.header) > 0 {
+		httpReq.Header = req.header
+	}
+	if req.offset > 0 {
+		// TODO: implement range requests
+		return ErrNotImplemented
+	}
+
+	resp, err := req.r.httpClient.Do(httpReq)
+	if err != nil {
+		return err
+	}
+	req.responses = append(req.responses, resp)
+
+	return nil
+}
+
+func (req *request) checkResp() error {
+	if len(req.responses) == 0 {
+		return ErrNotFound
+	}
+	lastResp := req.responses[len(req.responses)-1]
+	if lastResp.StatusCode == http.StatusUnauthorized {
+		if req.r.auth != nil {
+			if err := req.r.auth.HandleResponse(lastResp); err != nil {
+				return err
+			}
+			return ErrRetryNeeded
+		}
+		return ErrUnauthorized
+	} else if lastResp.StatusCode == http.StatusRequestTimeout || lastResp.StatusCode == http.StatusTooManyRequests {
+		// TODO: backoff, next mirror
+
+	} else if lastResp.StatusCode >= 200 && lastResp.StatusCode < 300 {
+		return nil
+	}
+
+	switch lastResp.StatusCode {
+
+	case http.StatusRequestTimeout, http.StatusTooManyRequests:
+
+	default:
+	}
+
+	return ErrNotImplemented
+}
+
+func (req *request) backoff(removeMirror bool) error {
+	if req.backoffs == req.r.limit {
+		return ErrBackoffLimit
+	}
+	// sleep for backoff time
+	sleepTime := req.r.delayInit << req.backoffs
+	if sleepTime > req.r.delayMax {
+		sleepTime = req.r.delayMax
+	}
+	fmt.Fprintf(os.Stderr, "Backoff for %d seconds.\n", sleepTime.Seconds())
+	time.Sleep(sleepTime)
+	// next mirror based on whether remove flag is set
+	if removeMirror {
+		req.urls = append(req.urls[:req.curURL], req.urls[req.curURL+1:]...)
+	} else {
+		req.curURL = (req.curURL + 1) % len(req.urls)
+	}
+	return nil
+}
+
+func (req *request) Read(b []byte) (int, error) {
+	// if done, return eof
+	if req.done {
+		return 0, io.EOF
+	}
+	// if no responses, error
+	if len(req.responses) == 0 {
+		return 0, ErrNotFound
+	}
+	// fetch block
+	lastResp := req.responses[len(req.responses)-1]
+	i, err := lastResp.Body.Read(b)
+	req.curRead += int64(i)
+	// add to digest
+	if req.digester != nil && i > 0 {
+		req.digester.Hash().Write(b)
+	}
+	if err == io.EOF && lastResp.ContentLength > 0 {
+		// handle early EOF or other failed connection with a retry from an offset
+		if req.curRead < lastResp.ContentLength {
+			req.offset += req.curRead
+			for {
+				err = req.httpDo()
+				if err != nil {
+					return i, err
+				}
+				err = req.checkResp()
+				if err == nil {
+					break
+				} else if err != ErrRetryNeeded {
+					return i, err
+				}
+			}
+		} else if req.curRead >= lastResp.ContentLength {
+			req.done = true
+		}
+	}
+	// if eof, verify digest, set error on mismatch
+	if req.digester != nil && err == io.EOF && req.digest != req.digester.Digest() {
+		req.done = true
+		return i, ErrDigestMismatch
+	}
+
+	// pass through read on the last response
+	return i, err
+}
+
+func (req *request) Close() error {
+	// if no responses, error
+	if len(req.responses) == 0 {
+		return ErrNotFound
+	}
+	// pass through close to last request, mark as done
+	req.done = true
+	return req.responses[len(req.responses)-1].Body.Close()
+}
+
+func (req *request) HTTPResponse() *http.Response {
+	if len(req.responses) > 0 {
+		return req.responses[len(req.responses)-1]
+	}
+	return nil
+}
+
+func (req *request) HTTPResponses() ([]*http.Response, error) {
+	if len(req.responses) > 0 {
+		return req.responses, nil
+	}
+	return nil, ErrNotFound
+}
