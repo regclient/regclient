e517ecf7d7f30f5870207caf7e69ec56b9923596
Brandon Mitchell <git@bmitch.net>
2020-09-21 16:25:21 -0400

    Adding hidden digest command
    This is effectively a sha256sum command, leaving it hidden for now while
    deciding if it should stay.

---
+++ cmd/regctl/digest.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/opencontainers/go-digest"
+	"github.com/spf13/cobra"
+)
+
+// TODO: identify a more appropriate location for this command, leave it hidden until then
+var digestCmd = &cobra.Command{
+	Hidden: true,
+	Use:    "digest",
+	Short:  "compute digest on stdin",
+	Args:   cobra.RangeArgs(0, 0),
+	RunE:   runDigest,
+}
+
+func init() {
+	rootCmd.AddCommand(digestCmd)
+}
+
+func runDigest(cmd *cobra.Command, args []string) error {
+	digester := digest.Canonical.Digester()
+
+	_, err := io.Copy(digester.Hash(), os.Stdin)
+
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(digester.Digest().String())
+	return nil
+}
