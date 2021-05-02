ee7dd2570130c8cf8ea7d2ef14fa101f678b4c66
Brandon Mitchell <git@bmitch.net>
2021-05-02 19:49:06 -0400

    Check blob size and digest on read
    Signed-off-by: Brandon Mitchell <git@bmitch.net>

--- regclient/blob/reader.go
+++ regclient/blob/reader.go
@@ -6,7 +6,9 @@ import (
 	"io"
 	"io/ioutil"
 
+	"github.com/opencontainers/go-digest"
 	ociv1 "github.com/opencontainers/image-spec/specs-go/v1"
+	"github.com/regclient/regclient/regclient/types"
 )
 
 // Reader is an unprocessed Blob with an available ReadCloser for reading the Blob
@@ -19,23 +21,34 @@ type Reader interface {
 // reader is the internal struct implementing BlobReader
 type reader struct {
 	common
-	io.ReadCloser
+	readBytes int64
+	reader    io.Reader
+	closer    io.Closer
+	digester  digest.Digester
+	// io.ReadCloser
 }
 
-// TODO: wrap the ReadCloser, running all reads through a digester
-// on final EOF, either save the digest and length, or report an error if the length and/or digest doesn't match
-
 // NewReader creates a new reader
-func NewReader(rc io.ReadCloser) Reader {
-	bc := common{}
-	if rc != nil {
+func NewReader(rdr io.ReadCloser) Reader {
+	digester := digest.Canonical.Digester()
+	digestRdr := io.TeeReader(rdr, digester.Hash())
+	bc := common{
+		ref: types.Ref{},
+	}
+	if rdr != nil {
 		bc.blobSet = true
 	}
 	br := reader{
-		common:     bc,
-		ReadCloser: rc,
+		common:   bc,
+		reader:   digestRdr,
+		closer:   rdr,
+		digester: digester,
 	}
 	return &br
+}
+
+func (b *reader) Close() error {
+	return b.closer.Close()
 }
 
 // RawBody returns the original body from the request
@@ -43,12 +56,35 @@ func (b *reader) RawBody() ([]byte, error) {
 	return ioutil.ReadAll(b)
 }
 
+// Read passes through the read operation while computing the digest and tracking the size
+func (b *reader) Read(p []byte) (int, error) {
+	size, err := b.reader.Read(p)
+	b.readBytes = b.readBytes + int64(size)
+	if err == io.EOF {
+		// check/save size
+		if b.cl == 0 {
+			b.cl = b.readBytes
+		} else if b.readBytes != b.cl {
+			err = fmt.Errorf("Expected size mismatch [expected %d, received %d]: %w", b.cl, b.readBytes, err)
+		}
+		// check/save digest
+		if b.digest == "" {
+			b.digest = b.digester.Digest()
+		} else if b.digest != b.digester.Digest() {
+			err = fmt.Errorf("Expected digest mismatch [expected %s, calculated %s]: %w", b.digest.String(), b.digester.Digest().String(), err)
+		}
+	}
+	return size, err
+}
+
 // ToOCIConfig converts a blobReader to a BlobOCIConfig
 func (b *reader) ToOCIConfig() (OCIConfig, error) {
 	if !b.blobSet {
 		return nil, fmt.Errorf("Blob is not defined")
 	}
-	// TODO: error if blob read has already been called
+	if b.readBytes != 0 {
+		return nil, fmt.Errorf("Unable to convert after read has been performed")
+	}
 	blobBody, err := ioutil.ReadAll(b)
 	if err != nil {
 		return nil, fmt.Errorf("Error reading image config for %s: %w", b.ref.CommonName(), err)
