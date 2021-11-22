77f865d44884b9b88f602e1d3de49124c3887bbd
Brandon Mitchell <git@bmitch.net>
2021-11-22 15:09:33 -0500

    refactor refs to support OCI layout
    Signed-off-by: Brandon Mitchell <git@bmitch.net>

--- regclient/types/ref.go
+++ regclient/types/ref.go
@@ -1,6 +1,21 @@
 package types
 
-import "github.com/docker/distribution/reference"
+import (
+	"fmt"
+	"regexp"
+
+	"github.com/docker/distribution/reference"
+)
+
+var (
+	pathS    = `[/a-zA-Z0-9_\-. ]+`
+	tagS     = `[\w][\w.-]{0,127}`
+	digestS  = `[A-Za-z][A-Za-z0-9]*(?:[-_+.][A-Za-z][A-Za-z0-9]*)*[:][[:xdigit:]]{32,}`
+	schemeRE = regexp.MustCompile(`^([a-z]+)://(.+)$`)
+	pathRE   = regexp.MustCompile(`^(` + pathS + `)` +
+		`(?:` + regexp.QuoteMeta(`:`) + `(` + tagS + `))?` +
+		`(?:` + regexp.QuoteMeta(`@`) + `(` + digestS + `))?$`)
+)
 
 // Ref reference to a registry/repository
 // If the tag or digest is available, it's also included in the reference.
@@ -8,35 +23,63 @@ import "github.com/docker/distribution/reference"
 // While this is currently a struct, that may change in the future and access
 // to contents should not be assumed/used.
 type Ref struct {
-	Reference, Registry, Repository, Tag, Digest string
+	Scheme     string
+	Reference  string // unparsed string
+	Registry   string // server, host:port
+	Repository string // path on server
+	Tag        string
+	Digest     string
+	Path       string
 }
 
-// NewRef returns a repository reference including a registry, repository (path), digest, and tag
+// NewRef returns a reference based on the scheme, defaulting to a
 func NewRef(ref string) (Ref, error) {
-	parsed, err := reference.ParseNormalizedNamed(ref)
-
-	var ret Ref
-	ret.Reference = ref
-
-	if err != nil {
-		return ret, err
+	scheme := ""
+	path := ref
+	matchScheme := schemeRE.FindStringSubmatch(ref)
+	if matchScheme != nil && len(matchScheme) == 3 {
+		scheme = matchScheme[1]
+		path = matchScheme[2]
 	}
-
-	ret.Registry = reference.Domain(parsed)
-	ret.Repository = reference.Path(parsed)
-
-	if canonical, ok := parsed.(reference.Canonical); ok {
-		ret.Digest = canonical.Digest().String()
+	ret := Ref{
+		Scheme:    scheme,
+		Reference: ref,
 	}
+	switch scheme {
+	case "":
+		ret.Scheme = "reg"
+		parsed, err := reference.ParseNormalizedNamed(ref)
+		if err != nil {
+			return ret, err
+		}
+		ret.Registry = reference.Domain(parsed)
+		ret.Repository = reference.Path(parsed)
+		if canonical, ok := parsed.(reference.Canonical); ok {
+			ret.Digest = canonical.Digest().String()
+		}
+		if tagged, ok := parsed.(reference.Tagged); ok {
+			ret.Tag = tagged.Tag()
+		}
+		if ret.Tag == "" && ret.Digest == "" {
+			ret.Tag = "latest"
+		}
 
-	if tagged, ok := parsed.(reference.Tagged); ok {
-		ret.Tag = tagged.Tag()
+	case "ocidir", "ocifile":
+		matchPath := pathRE.FindStringSubmatch(path)
+		if matchPath == nil || len(matchPath) < 2 || matchPath[1] == "" {
+			return Ref{}, fmt.Errorf("invalid path for scheme \"%s\": %s", scheme, path)
+		}
+		ret.Path = matchPath[1]
+		if len(matchPath) > 2 && matchPath[2] != "" {
+			ret.Tag = matchPath[2]
+		}
+		if len(matchPath) > 3 && matchPath[3] != "" {
+			ret.Digest = matchPath[3]
+		}
+
+	default:
+		return Ref{}, fmt.Errorf("unhandled reference scheme \"%s\" in \"%s\"", scheme, ref)
 	}
-
-	if ret.Tag == "" && ret.Digest == "" {
-		ret.Tag = "latest"
-	}
-
 	return ret, nil
 }
 
---
+++ regclient/types/ref_test.go
@@ -0,0 +1,148 @@
+package types
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestRef(t *testing.T) {
+	var tests = []struct {
+		name       string
+		ref        string
+		scheme     string
+		registry   string
+		repository string
+		tag        string
+		digest     string
+		path       string
+		wantE      error
+	}{
+		{
+			name:       "Docker library",
+			ref:        "alpine",
+			scheme:     "reg",
+			registry:   "docker.io",
+			repository: "library/alpine",
+			tag:        "latest",
+			digest:     "",
+			path:       "",
+			wantE:      nil,
+		},
+		{
+			name:       "Local registry",
+			ref:        "localhost:5000/group/image:v42",
+			scheme:     "reg",
+			registry:   "localhost:5000",
+			repository: "group/image",
+			tag:        "v42",
+			digest:     "",
+			path:       "",
+			wantE:      nil,
+		},
+		{
+			name:       "OCI file",
+			ref:        "ocifile://path/to/file.tgz",
+			scheme:     "ocifile",
+			registry:   "",
+			repository: "",
+			tag:        "",
+			digest:     "",
+			path:       "path/to/file.tgz",
+			wantE:      nil,
+		},
+		{
+			name:       "OCI file with tag",
+			ref:        "ocifile://path/to/file.tgz:v1.2.3",
+			scheme:     "ocifile",
+			registry:   "",
+			repository: "",
+			tag:        "v1.2.3",
+			digest:     "",
+			path:       "path/to/file.tgz",
+			wantE:      nil,
+		},
+		{
+			name:       "OCI file with digest",
+			ref:        "ocifile://path/to/file.tgz@sha256:15f840677a5e245d9ea199eb9b026b1539208a5183621dced7b469f6aa678115",
+			scheme:     "ocifile",
+			registry:   "",
+			repository: "",
+			tag:        "",
+			digest:     "sha256:15f840677a5e245d9ea199eb9b026b1539208a5183621dced7b469f6aa678115",
+			path:       "path/to/file.tgz",
+			wantE:      nil,
+		},
+		{
+			name:  "OCI file with invalid digest",
+			ref:   "ocifile://path/to/file.tgz@sha256:ZZ15f840677a5e245d9ea199eb9b026b1539208a5183621dced7b469f6aa678115ZZ",
+			wantE: fmt.Errorf("invalid path for scheme \"ocifile\": path/to/file.tgz@sha256:ZZ15f840677a5e245d9ea199eb9b026b1539208a5183621dced7b469f6aa678115ZZ"),
+		},
+		{
+			name:       "OCI dir",
+			ref:        "ocidir://path/to/dir",
+			scheme:     "ocidir",
+			registry:   "",
+			repository: "",
+			tag:        "",
+			digest:     "",
+			path:       "path/to/dir",
+			wantE:      nil,
+		},
+		{
+			name:       "OCI dir with tag",
+			ref:        "ocidir://path/to/dir:v1.2.3",
+			scheme:     "ocidir",
+			registry:   "",
+			repository: "",
+			tag:        "v1.2.3",
+			digest:     "",
+			path:       "path/to/dir",
+			wantE:      nil,
+		},
+		{
+			name:       "OCI dir with digest",
+			ref:        "ocidir://path/to/dir@sha256:15f840677a5e245d9ea199eb9b026b1539208a5183621dced7b469f6aa678115",
+			scheme:     "ocidir",
+			registry:   "",
+			repository: "",
+			tag:        "",
+			digest:     "sha256:15f840677a5e245d9ea199eb9b026b1539208a5183621dced7b469f6aa678115",
+			path:       "path/to/dir",
+			wantE:      nil,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ref, err := NewRef(tt.ref)
+			if tt.wantE == nil && err != nil {
+				t.Errorf("failed creating reference, err: %v", err)
+				return
+			} else if tt.wantE != nil && (err == nil || (tt.wantE != err && tt.wantE.Error() != err.Error())) {
+				t.Errorf("expected error not received, expected %v, received %v", tt.wantE, err)
+				return
+			} else if tt.wantE != nil {
+				return
+			}
+			if tt.scheme != ref.Scheme {
+				t.Errorf("scheme mismatch for %s, expected %s, received %s", tt.ref, tt.scheme, ref.Scheme)
+			}
+			if tt.registry != ref.Registry {
+				t.Errorf("registry mismatch for %s, expected %s, received %s", tt.ref, tt.registry, ref.Registry)
+			}
+			if tt.repository != ref.Repository {
+				t.Errorf("repository mismatch for %s, expected %s, received %s", tt.ref, tt.repository, ref.Repository)
+			}
+			if tt.tag != ref.Tag {
+				t.Errorf("tag mismatch for %s, expected %s, received %s", tt.ref, tt.tag, ref.Tag)
+			}
+			if tt.digest != ref.Digest {
+				t.Errorf("digest mismatch for %s, expected %s, received %s", tt.ref, tt.digest, ref.Digest)
+			}
+			if tt.path != ref.Path {
+				t.Errorf("path mismatch for %s, expected %s, received %s", tt.ref, tt.path, ref.Path)
+			}
+
+		})
+	}
+}
