263d56b06f53d2d66ba44c40d3777d1cd77fd94b
Brandon Mitchell <git@bmitch.net>
2022-02-03 20:20:08 -0500

    Retryable linting
    Signed-off-by: Brandon Mitchell <git@bmitch.net>

--- internal/retryable/error.go
+++ internal/retryable/error.go
@@ -1,22 +1,25 @@
+//go:build !nolegacy
+// +build !nolegacy
+
 package retryable
 
 import "errors"
 
 var (
 	// ErrAllRequestsFailed when there are no mirrors left to try
-	ErrAllRequestsFailed = errors.New("All requests failed")
+	ErrAllRequestsFailed = errors.New("all requests failed")
 	// ErrBackoffLimit maximum backoff attempts reached
-	ErrBackoffLimit = errors.New("Backoff limit reached")
+	ErrBackoffLimit = errors.New("backoff limit reached")
 	// ErrCanceled if the context was canceled
-	ErrCanceled = errors.New("Context was canceled")
+	ErrCanceled = errors.New("context was canceled")
 	// ErrDigestMismatch if the expected digest wasn't received
-	ErrDigestMismatch = errors.New("Digest mismatch")
+	ErrDigestMismatch = errors.New("digest mismatch")
 	// ErrNotFound isn't there, search for your value elsewhere
-	ErrNotFound = errors.New("Not found")
+	ErrNotFound = errors.New("not found")
 	// ErrNotImplemented returned when method has not been implemented yet
-	ErrNotImplemented = errors.New("Not implemented")
+	ErrNotImplemented = errors.New("not implemented")
 	// ErrRetryNeeded indicates a request needs to be retried
-	ErrRetryNeeded = errors.New("Retry needed")
+	ErrRetryNeeded = errors.New("retry needed")
 	// ErrUnauthorized request was not authorized
-	ErrUnauthorized = errors.New("Unauthorized")
+	ErrUnauthorized = errors.New("unauthorized")
 )
