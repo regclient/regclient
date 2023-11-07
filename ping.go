fde4eb51027d70212d2840a814433706f74cf1b5
Nikolay Edigaryev <edigaryev@gmail.com>
2023-11-07 11:18:56 +0400

    Switch repoPinger interface to ref.Ref
    Signed-off-by: Nikolay Edigaryev <edigaryev@gmail.com>

--- ping.go
+++ ping.go
@@ -2,31 +2,27 @@ package regclient
 
 import (
 	"context"
-	"fmt"
-	"strings"
-
 	"github.com/regclient/regclient/types"
+	"github.com/regclient/regclient/types/ref"
 )
 
 type repoPinger interface {
-	Ping(ctx context.Context, hostname string) error
+	Ping(ctx context.Context, r ref.Ref) error
 }
 
 // Ping accesses the registry's root endpoint, which allows to determine whether
 // the registry implements the OCI Distribution Specification and whether the
 // registry accepts the supplied credentials.
-func (rc *RegClient) Ping(ctx context.Context, hostname string) error {
-	i := strings.Index(hostname, "/")
-	if i > 0 {
-		return fmt.Errorf("invalid hostname: %s%.0w", hostname, types.ErrParsingFailed)
-	}
-	schemeAPI, err := rc.schemeGet("reg")
+func (rc *RegClient) Ping(ctx context.Context, r ref.Ref) error {
+	schemeAPI, err := rc.schemeGet(r.Scheme)
 	if err != nil {
 		return err
 	}
+
 	rp, ok := schemeAPI.(repoPinger)
 	if !ok {
 		return types.ErrNotImplemented
 	}
-	return rp.Ping(ctx, hostname)
+
+	return rp.Ping(ctx, r)
 }
--- scheme/reg/ping.go
+++ scheme/reg/ping.go
@@ -4,11 +4,12 @@ import (
 	"context"
 	"fmt"
 	"github.com/regclient/regclient/internal/reghttp"
+	"github.com/regclient/regclient/types/ref"
 )
 
-func (reg *Reg) Ping(ctx context.Context, hostname string) error {
+func (reg *Reg) Ping(ctx context.Context, r ref.Ref) error {
 	req := &reghttp.Req{
-		Host:      hostname,
+		Host:      r.Registry,
 		NoMirrors: true,
 		APIs: map[string]reghttp.ReqAPI{
 			"": {
@@ -17,13 +18,16 @@ func (reg *Reg) Ping(ctx context.Context, hostname string) error {
 			},
 		},
 	}
+
 	resp, err := reg.reghttp.Do(ctx, req)
 	if err != nil {
-		return fmt.Errorf("failed to ping registry %s: %w", hostname, err)
+		return fmt.Errorf("failed to ping registry %s: %w", r.Registry, err)
 	}
 	defer resp.Close()
+
 	if resp.HTTPResponse().StatusCode != 200 {
-		return fmt.Errorf("failed to ping registry %s: %w", hostname, reghttp.HTTPError(resp.HTTPResponse().StatusCode))
+		return fmt.Errorf("failed to ping registry %s: %w",
+			r.Registry, reghttp.HTTPError(resp.HTTPResponse().StatusCode))
 	}
 
 	return nil
