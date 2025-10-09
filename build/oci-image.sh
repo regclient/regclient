672c71096b9ed05e0c81daaa5aff202b91665956
Brandon Mitchell <git@bmitch.net>
2025-10-09 09:06:53 -0400

    Version bump
    - Pin cosign to v2 pending an update to cosign-installer
    - Alpine to v3.22.2
    - regclient/actions to latest commit
    - golang.org/x/term to v0.36.0
    
    Signed-off-by: Brandon Mitchell <git@bmitch.net>

--- build/oci-image.sh
+++ build/oci-image.sh
@@ -7,7 +7,7 @@ base_name=""
 release="scratch"
 push_tags=""
 ALPINE_NAME="alpine:3"
-ALPINE_DIGEST="sha256:4bcff63911fcb4448bd4fdacec207030997caf25e9bea4045fa6c8c44de311d1" # 3.22.1
+ALPINE_DIGEST="sha256:4b7ce07002c69e8f3d704a9c5d6fd3053be500b7f1c69fc0d80990c2ad8dd412" # 3.22.2
 
 # CLI options to override image, platform, base digest, and comma separated list of tags to push
 opt_c=0
