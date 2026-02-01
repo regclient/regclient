e1731e577bc94dbb0dfa123aa1230f37c9d63229
Brandon Mitchell <git@bmitch.net>
2026-02-01 07:49:16 -0500

    Version bump
    - Alpine base image to v3.23.3
    - docker/login-action to v3.7.0
    - anchore/syft to v1.41.1
    - anchore/sbom-action to v0.22.1
    
    Signed-off-by: Brandon Mitchell <git@bmitch.net>

--- build/oci-image.sh
+++ build/oci-image.sh
@@ -7,7 +7,7 @@ base_name=""
 release="scratch"
 push_tags=""
 ALPINE_NAME="alpine:3"
-ALPINE_DIGEST="sha256:865b95f46d98cf867a156fe4a135ad3fe50d2056aa3f25ed31662dff6da4eb62" # 3.23.2
+ALPINE_DIGEST="sha256:25109184c71bdad752c8312a8623239686a9a2071e8825f20acb8f2198c3f659" # 3.23.3
 
 # CLI options to override image, platform, base digest, and comma separated list of tags to push
 opt_c=0
